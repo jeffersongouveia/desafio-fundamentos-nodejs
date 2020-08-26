05531291da8a67ab556dfef3c67116490a09d8cb
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-08-26 19:24:51 -0400

    ✅  All challenges passed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import Transaction from '../models/Transaction';
+import Transaction from '../models/Transaction'
 
 interface Balance {
   income: number;
@@ -6,24 +6,53 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+}
+
+interface ListTransactions {
+  transactions: Transaction[]
+  balance: Balance
+}
+
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[]
 
   constructor() {
-    this.transactions = [];
+    this.transactions = []
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): ListTransactions {
+    return {
+      balance: this.getBalance(),
+      transactions: [...this.transactions],
+    }
   }
 
   public getBalance(): Balance {
-    // TODO
+    const sumIncome = this.transactions.reduce((sum, item) => {
+      sum += item.type === 'income' ? Number(item.value) : 0
+      return sum
+    }, 0)
+    const sumOutcome = this.transactions.reduce((sum, item) => {
+      sum += item.type === 'outcome' ? Number(item.value) : 0
+      return sum
+    }, 0)
+
+    return {
+      income: sumIncome,
+      outcome: sumOutcome,
+      total: sumIncome - sumOutcome,
+    }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(data: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction(data)
+    this.transactions.push(transaction)
+    return transaction
   }
 }
 
-export default TransactionsRepository;
+export default TransactionsRepository
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,16 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute(data: Request): Transaction {
+    if (data.type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance()
+
+      if (balance.total < data.value) {
+        throw Error('You don\'t have credit for this transaction')
+      }
+    }
+
+    return this.transactionsRepository.create(data)
   }
 }
 
